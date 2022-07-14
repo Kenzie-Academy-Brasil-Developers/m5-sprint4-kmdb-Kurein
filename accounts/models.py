006d57b06a44cbc5de2d50ef890f06da12746736
Kaueh <kaueh1234@gmail.com>
2022-07-14 18:24:51 -0300

    Base user done

---
+++ accounts/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from .managers import UserManager
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
